fcb587a9b55c39c9d08cceb5ff8fa417f29881b5
Alexander Skornyakov <a1212sk@gmail.com>
2020-07-13 15:01:22 +0300

    add type converters: ByteArray <-> Bitmap

---
+++ app/src/main/java/alexander/skornyakov/rtracker/db/Converters.kt
@@ -0,0 +1,22 @@
+package alexander.skornyakov.rtracker.db
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.room.TypeConverter
+import java.io.ByteArrayOutputStream
+
+class Converters {
+
+    @TypeConverter
+    fun fromBitmap(bitmap: Bitmap): ByteArray{
+        val outputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+        return outputStream.toByteArray()
+    }
+
+    @TypeConverter
+    fun fromBytes(bytes: ByteArray): Bitmap{
+        return BitmapFactory.decodeByteArray(bytes,0, bytes.size)
+    }
+
+}
