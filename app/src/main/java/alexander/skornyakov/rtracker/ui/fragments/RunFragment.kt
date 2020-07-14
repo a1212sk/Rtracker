9411d3903ffbcdc37dbba27b0d43d4a91d559377
Alexander Skornyakov <a1212sk@gmail.com>
2020-07-14 16:11:43 +0300

    set up some navigation buttons

--- app/src/main/java/alexander/skornyakov/rtracker/ui/fragments/RunFragment.kt
+++ app/src/main/java/alexander/skornyakov/rtracker/ui/fragments/RunFragment.kt
@@ -2,13 +2,24 @@ package alexander.skornyakov.rtracker.ui.fragments
 
 import alexander.skornyakov.rtracker.R
 import alexander.skornyakov.rtracker.ui.viewmodels.MainViewModel
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_run.*
 
 @AndroidEntryPoint
 class RunFragment : Fragment(R.layout.fragment_run){
 
     private val viewModel: MainViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fab.setOnClickListener {
+            findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
+        }
+    }
 
 }
