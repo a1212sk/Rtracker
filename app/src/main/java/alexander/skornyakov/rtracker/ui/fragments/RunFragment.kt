0ae4f202fa80ea522790e587b9090e936b9cc606
Alexander Skornyakov <a1212sk@gmail.com>
2020-07-14 14:08:20 +0300

    provide ViewModels to the fragments

--- app/src/main/java/alexander/skornyakov/rtracker/ui/fragments/RunFragment.kt
+++ app/src/main/java/alexander/skornyakov/rtracker/ui/fragments/RunFragment.kt
@@ -1,8 +1,14 @@
 package alexander.skornyakov.rtracker.ui.fragments
 
 import alexander.skornyakov.rtracker.R
+import alexander.skornyakov.rtracker.ui.viewmodels.MainViewModel
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RunFragment : Fragment(R.layout.fragment_run){
+
+    private val viewModel: MainViewModel by viewModels()
 
 }
--- app/src/main/java/alexander/skornyakov/rtracker/ui/fragments/StatisticsFragment.kt
+++ app/src/main/java/alexander/skornyakov/rtracker/ui/fragments/StatisticsFragment.kt
@@ -1,8 +1,14 @@
 package alexander.skornyakov.rtracker.ui.fragments
 
 import alexander.skornyakov.rtracker.R
+import alexander.skornyakov.rtracker.ui.viewmodels.StatisticsViewModel
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class StatisticsFragment : Fragment(R.layout.fragment_statistics){
+
+    private val viewModel: StatisticsViewModel by viewModels()
 
 }
