330b0b1fc8724f278e759664dc6eee9112d0c626
Alexander Skornyakov <a1212sk@gmail.com>
2020-07-14 11:46:00 +0300

    add db instance provider

--- app/src/main/java/alexander/skornyakov/rtracker/di/AppModule.kt
+++ app/src/main/java/alexander/skornyakov/rtracker/di/AppModule.kt
@@ -1,11 +1,24 @@
 package alexander.skornyakov.rtracker.di
 
+import alexander.skornyakov.rtracker.db.RunningDatabase
+import alexander.skornyakov.rtracker.helpers.Constants.DATABASE_NAME
+import android.content.Context
+import androidx.room.Room
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
 
 @Module
 @InstallIn(ApplicationComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideRunningDatabase(@ApplicationContext context: Context) =
+        Room.databaseBuilder(context, RunningDatabase::class.java, DATABASE_NAME)
+            .build()
 
 }
---
+++ app/src/main/java/alexander/skornyakov/rtracker/helpers/Constants.kt
@@ -0,0 +1,5 @@
+package alexander.skornyakov.rtracker.helpers
+
+object Constants {
+    const val DATABASE_NAME = "running_db"
+}
