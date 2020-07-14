584d38e1f4ffc9f73bb456e43710ebff7a82c673
Alexander Skornyakov <a1212sk@gmail.com>
2020-07-14 12:04:58 +0300

    checkout if the instance of RunDao has been created

--- app/src/main/java/alexander/skornyakov/rtracker/MainActivity.kt
+++ app/src/main/java/alexander/skornyakov/rtracker/MainActivity.kt
@@ -1,11 +1,21 @@
 package alexander.skornyakov.rtracker
 
+import alexander.skornyakov.rtracker.db.RunDao
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var runDao: RunDao
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("RunDao", "${runDao.hashCode()}")
     }
 }
