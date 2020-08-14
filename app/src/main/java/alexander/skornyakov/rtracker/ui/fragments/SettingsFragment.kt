1e90ec53ce61224da5e3ee3eebcef38db9022145
Alexander Skornyakov <a1212sk@gmail.com>
2020-08-14 16:01:18 +0300

    implement saving username and weight in SettingsFragment

--- app/src/main/java/alexander/skornyakov/rtracker/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/alexander/skornyakov/rtracker/ui/fragments/SettingsFragment.kt
@@ -1,8 +1,57 @@
 package alexander.skornyakov.rtracker.ui.fragments
 
 import alexander.skornyakov.rtracker.R
+import alexander.skornyakov.rtracker.helpers.Constants.KEY_NAME
+import alexander.skornyakov.rtracker.helpers.Constants.KEY_WEIGHT
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_settings.*
+import javax.inject.Inject
 
-class SettingsFragment : Fragment(R.layout.fragment_settings){
+@AndroidEntryPoint
+class SettingsFragment : Fragment(R.layout.fragment_settings) {
+
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnApplyChanges.setOnClickListener {
+            val success = applyChangesToSharedPref()
+            if (success) {
+                Snackbar.make(view, "Saved changes", Snackbar.LENGTH_LONG).show()
+            } else {
+                Snackbar.make(view, "Please fill out the fields!", Snackbar.LENGTH_LONG)
+                    .show()
+            }
+        }
+    }
+
+    private fun applyChangesToSharedPref(): Boolean {
+        val nameText = etName.text.toString()
+        val weight = etWeight.text.toString()
+        if (nameText.isEmpty() || weight.isEmpty()) {
+            return false
+        }
+        sharedPreferences.edit()
+            .putString(KEY_NAME, nameText)
+            .putFloat(KEY_WEIGHT, weight.toFloat())
+            .apply()
+        val toolbarText = "Let's go $nameText"
+        requireActivity().tvToolbarTitle.text = toolbarText
+        return true
+    }
+
+    private fun loadFieldsFromSharePrefs() {
+        val name = sharedPreferences.getString(KEY_NAME, "")
+        val weight = sharedPreferences.getFloat(KEY_WEIGHT, 80f)
+        etName.setText(name)
+        etWeight.setText(weight.toString())
+    }
 
 }
